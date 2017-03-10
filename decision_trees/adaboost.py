bd658ae27b2d4e139358f934cbfe0f4d0f8fe56e
sayantan <4auddy@informatik.uni-hamburg.de>
2017-03-10 20:04:12 +0100

    Updated adaboost.py to handle multiclass classification

--- decision_trees/adaboost.py
+++ decision_trees/adaboost.py
@@ -3,28 +3,46 @@
 Created on Thu Mar  9 17:26:42 2017
 
 @author: sayantan
-Implementation of the Adaboost algorithm (binary classification with numeric 
-attributes). This program is based on 'AdaBoost and the Super Bowl of 
-Classifiers: A Tutorial Introduction to Adaptive Boosting' by Rojas.
+Implementation of the Adaboost algorithm (classification with numeric 
+attributes only). Both binary and multiclass classification is handled
+
+This program is based on the following paper (for binary classification):
+'AdaBoost and the Super Bowl of Classifiers: A Tutorial Introduction to 
+Adaptive Boosting' - Rojas.
+
+The following changes have been made to the algorithm in the above paper (for
+handling multiclass classification):
+
+    (1)    Labels are not converted to +1 or -1, they are kept as is
+    (2)    The classification decision is not taken by the sign of C(x), but
+           instead is taken to be the class that was given the maximum weight
+           by the committee of classifiers
+    (3)    Weights of the data points are normalized
+    (4)    Multiclass Adaptive boosting is also handled
+    (5)    Best classifier's weight is set as:
+           best_classifier_weight = np.log((1-e_m)/e_m) + np.log(num_classes - 1)
+           This handles both binary and multi-class classification
+           
+These changes are based on the following paper:
+'Multi-class AdaBoost' - Zhu, Zou, Rosset, Hastie
+
 The breast cancer wisconsin dataset has been used
 [https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/]
 """
 
 import numpy as np
+import operator
 from cart import split_dataset, cart, classify
 
 def readdata_bc(datafile):
     """
     Reads the data file and formats it into attributes and classes
-    The dataset at https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/
-    is used.
-    Last column is the label which is denoted by  4: malignant and 2: benign
-    This is changed to 1: malignant and -1: benign
+    Rows with missing attributes are skipped
     Input:
         datafile: string containing the full path of the data file
     Output:
         (attributes, classes): list of attributes and classes
-        attributes is a list of lists, where each row is a data point
+        attributes is a list of lists, where each row is a data point,
         classes contains the label for the corresponding attribute row
     """
     
@@ -42,25 +60,23 @@ def readdata_bc(datafile):
             # Only consider data points which do not have any missing attributes
             if '?' not in line:
                 attributes.append(map(float,line.split(',')[1:9]))
-                if line.split(',')[10]=='2':
-                    classes.append(-1)
-                elif line.split(',')[10]=='4':
-                    classes.append(1)
+                classes.append(line.split(',')[10])
                         
     return (attributes, classes)
     
 
-def adaboost(training_data, pool_size, subsample_ratio, committee_size):
+def adaboost(training_data, pool_size, subsample_ratio, committee_size, num_classes):
     """
-    Implementation of the Apative Boosting algorithm (based on )
+    Implementation of the Apative Boosting algorithm (binary and multiclass classification)
     Input:
         training_data: Training data consisting of (attributes, classes)
         where attributes is a 2D list with each data point in a row, classes
         is a 1D list containing the labels of the corresponding rows
         pool_size: Number of classifiers to be part of the pool
-        subsample_ratio: The ratio of training data (randomly picked) that 
+        subsample_ratio: The fraction of training data (randomly picked) that 
         should be used to train each weak classifier in the pool
         committee_size: Number of classifiers which take the final decision
+        num_classes: Number of target classes
     Output:
         chosen_classifiers: List of classifiers where each element is a 
         tuple (classifier, weight_of_classifier). Here classifier is a CART tree
@@ -100,6 +116,9 @@ def adaboost(training_data, pool_size, subsample_ratio, committee_size):
     # Initialize the weight matrix for the data points
     weight_matrix_data = np.ones((len(training_data[0]),1))
     
+    # Normalize the weights
+    weight_matrix_data = weight_matrix_data/sum(weight_matrix_data)
+    
     # Initialize a list to hold chosen classifiers
     # This list holds tuples of (classifier, weight)
     chosen_classifiers = []
@@ -120,7 +139,7 @@ def adaboost(training_data, pool_size, subsample_ratio, committee_size):
         e_m = W_e/W
         
         # Calculate the weight of this classifier towards the final decision
-        best_classifier_weight = 0.5*np.log((1-e_m)/e_m)
+        best_classifier_weight = np.log((1-e_m)/e_m) + np.log(num_classes - 1)
         
         # Add this classifier to the list of chosen classifiers
         chosen_classifiers.append((classifier_pool[best_classifier_index], best_classifier_weight))
@@ -134,46 +153,71 @@ def adaboost(training_data, pool_size, subsample_ratio, committee_size):
                 # For a miss
                 weight_matrix_data[index,0] =  weight_matrix_data[index,0] * np.sqrt((1-e_m)/e_m)
             else:
-                weight_matrix_data[index,0] =  weight_matrix_data[index,0] * np.sqrt(e_m/(1-e_m))                
-
-        # Now classification can be done by the committee of chosen classifiers
-        return chosen_classifiers
+                weight_matrix_data[index,0] =  weight_matrix_data[index,0] * np.sqrt(e_m/(1-e_m))       
+                
+        # Normalize the weight matrix
+        weight_matrix_data = weight_matrix_data/sum(weight_matrix_data)
+        
+    # Now classification can be done by the committee of chosen classifiers
+    return chosen_classifiers
             
-
+            
+def ensemble_classify(committee_of_classifiers, data_point):
+    """
+    Classifies a data point by a committee of classifiers
+    Input:
+        committee_of_classifiers: A list of classifiers, each element is a tuple
+        (classifier, weight) where classifier is a CART tree (in the form of a dict)
+        as used in cart.py and weight is a scalar. 
+        data_point: Row of attributes
+    Output:
+        classification_label: The classification result
+    """    
+    classify_result_vote = {}
+    for classifier in committee_of_classifiers:
+        # Each classifier votes for a particular class
+        # Weight of this vote = weight of the classifier
+        label = classify(data_point,classifier[0])
+        if label not in classify_result_vote.keys():
+            classify_result_vote[label] = classifier[1]
+        else:
+            classify_result_vote[label] += classifier[1]
+        
+    # After voting is complete by the committee of classifiers, the class with the maximum
+    # votes is taken to be the winner
+    classification_label = max(classify_result_vote.iteritems(), key=operator.itemgetter(1))[0]
+    return classification_label
+    
+    
 def main():
     """
     Main function.
     Divides the breast canser data set into training and testing set (80-20 ratio).
     Note:
         (i)    Here all attributes are numeric (integers)
-        (ii)   Only binary classification is handled here
-        (iii)  Labels are +1 for malignant and -1 for benign (handled by readdata function)
-        (iv)   Attributes with missing data are skipped (handled by readdata function)
+        (ii)   Attributes with missing data are skipped (handled by readdata function)
     """
     # Specify the data file
     datafile = '/home/sayantan/computing/datasets/uci_breastcanser_wisconsin_originial/breast-cancer-wisconsin.data'
     
-    # Split the dataset into training and test in the ratio 80-20
+    # Split the dataset into training and test sets in the ratio 80-20
     (training_data, test_data) = split_dataset(readdata_bc(datafile), 0.8)
 
-    # Use the training data to get an ensemble of weighted classifiers
-    chosen_classifiers = adaboost(training_data, pool_size=50, subsample_ratio=0.5, committee_size=10)    
+    # Use the training data to train an ensemble of weighted classifiers
+    committee_of_classifiers = adaboost(training_data, pool_size=50, subsample_ratio=0.5, committee_size=10, num_classes=2)    
     
     # Classify the test data using the weighted classifiers and calculate the error
     # Total error is just the 1-0 loss
     err_count = 0
+    
     for i in range(len(test_data[0])):
-        cumulative_sum = 0
-        for classifier in chosen_classifiers:
-            # cumulative_sum = wt1*classify(row, classifier1) + ....+ wtm*classify(row, classifierm)
-            cumulative_sum += classifier[1]*classify(test_data[0][i],classifier[0])
-            
-        classification_result = np.sign(cumulative_sum)
-            
-        if test_data[1][i] != classification_result:
+        # Classify a row of test data
+        classification_result = ensemble_classify(committee_of_classifiers, test_data[0][i])
+        # Check the result      
+        if test_data[1][i] != classification_result[0]:
             err_count += 1.0
             
-    # Display the accuracy as a percentage        
+    # Display the overall accuracy as a percentage        
     accuracy = 1.0 - float(err_count)/len(test_data[0])
     print str(accuracy*100) + '%'
 
